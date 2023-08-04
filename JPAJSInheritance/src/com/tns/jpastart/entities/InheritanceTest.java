17943c9cb4ae2ef6f00e3c319d2f66a2d6f79b55
DELL <DELL@DESKTOP-MB912FO>
2023-08-04 22:41:10 +0530

    joint table inheritance

---
+++ JPAJSInheritance/src/com/tns/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,36 @@
+package com.tns.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("Mounika");
+		employee.setSalary(45000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Shiva Teja");
+		manager.setSalary(120000);
+		manager.setDepartmentName("Mechanical");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to the  database.");
+		em.close();
+		factory.close();
+	}
+}
